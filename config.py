c9363ccd2b8dbc50845ed8ae54d68395735919cf
Nbt guy <93465296+Nbtguyoriginal@users.noreply.github.com>
2023-09-08 23:41:30 -0400

    Highlight error fix
    My wife noticed a bug in the highlight lol easy fix
    
    -def setup_text_tags():
        analysis_text.tag_configure('sending', background=config.SENDING_COLOR, font=("Arial", 10))
        analysis_text.tag_configure('analysis', background=config.ANALYSIS_COLOR, font=("Arial", 10))
        analysis_text.tag_configure('title', background=config.TITLE_COLOR, font=("Arial", 10, 'bold'))
    
    -added to config for easy color changes

--- config.py
+++ config.py
@@ -4,16 +4,20 @@
 
 # OpenAI Configuration
 API_KEY = ''  # OpenAI API Key
-LINES_PER_API_CALL = 10  # Number of lines to send to OpenAI API in a single call # 1 is a 1 to 1 conversion of lines to tokens 
+LINES_PER_API_CALL = 20  # Number of lines to send to OpenAI API in a single call # 1 is a 1 to 1 conversion of lines to tokens 
 
 # UI Configuration
-HIGHLIGHT_COLOR = '83FFA1'  # Color used to highlight specific lines in the analysis log Different colors in Color_codes.txt
+HIGHLIGHT_COLOR = '#FF33A1'  # Color used to highlight specific lines in the analysis log Different colors in Color_codes.txt
+SENDING_COLOR = '#FFA1A1' #- Light red
+ANALYSIS_COLOR = '#83C1A1'  # Aquamarine
+TITLE_COLOR = '#FFFFE0'     # lightyellow
+
 
 # Logging Configuration
-LOG_FILE = 'analysis_log.txt'  # File where the analysis log is saved
+LOG_FILE = 'Oompa_loompa.txt'  # File where the analysis log is saved
 
 
 # setting the "lines_per_api_call" any lower then "10" is not advised unless your code is very compact or you have a very small script
 
 # the Default ammount for best functions is "10" but adjust this number if you find your code is not well described in the log enough or gui
-# Feel free to fork this reposstitory and add other Functions as i coded thispretty quick
+# Feel free to fork this reposstitory and add other Functions as i coded this pretty quick
