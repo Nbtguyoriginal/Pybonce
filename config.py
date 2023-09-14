2cb745c565a1899fcadfb6c2ac59cf817b7ab2de
Nbt guy <93465296+Nbtguyoriginal@users.noreply.github.com>
2023-09-14 00:26:06 -0400

    added log to config and specified difrent log styles

--- config.py
+++ config.py
@@ -4,20 +4,37 @@
 
 # OpenAI Configuration
 API_KEY = ''  # OpenAI API Key
-LINES_PER_API_CALL = 20  # Number of lines to send to OpenAI API in a single call # 1 is a 1 to 1 conversion of lines to tokens 
+LINES_PER_API_CALL = 20  # Number of lines to send to OpenAI API in a single call
 
 # UI Configuration
-HIGHLIGHT_COLOR = '#FF33A1'  # Color used to highlight specific lines in the analysis log Different colors in Color_codes.txt
-SENDING_COLOR = '#FFA1A1' #- Light red
+HIGHLIGHT_COLOR = '#FF33A1'  # Color used to highlight specific lines in the analysis log
+SENDING_COLOR = '#FFA1A1'  # Light red
 ANALYSIS_COLOR = '#83C1A1'  # Aquamarine
 TITLE_COLOR = '#FFFFE0'     # lightyellow
 
-
 # Logging Configuration
-LOG_FILE = 'Oompa_loompa.txt'  # File where the analysis log is saved
+LOG_FILE = 'Oompa_loompa.txt'  
+# File where the analysis log is saved
+
+# Define different types of logs
+LOG_TYPES = {
+    "default": "default",
+    "modifications": "modifications",
+    "notes": "notes"
+}
+
+# Set the current log type
+CURRENT_LOG_TYPE = LOG_TYPES["default"]
+
+
+# Note: setting the "lines_per_api_call" any lower than "10" is not advised unless your code is very compact or you have a very small script.
+
+# The default amount for best functions is "10", but adjust this number if you find your code is not well described in the log or GUI.
+# Feel free to fork this repository and add other functions as it was coded quickly.
 
 
 # setting the "lines_per_api_call" any lower then "10" is not advised unless your code is very compact or you have a very small script
 
 # the Default ammount for best functions is "10" but adjust this number if you find your code is not well described in the log enough or gui
-# Feel free to fork this reposstitory and add other Functions as i coded this pretty quick
+
+# Feel free to fork this reposstitory and add other Functions as I coded this pretty quick
